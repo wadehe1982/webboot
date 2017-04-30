96151b61a1e493e2c6b7a653db23d70d203b6e89
wei <ir_01@163.com>
2017-04-30 10:50:00 +0800

    use JpaRepository replace CrudRepository.

--- src/main/java/com/xxx/webboot/repository/StudentRepository.java
+++ src/main/java/com/xxx/webboot/repository/StudentRepository.java
@@ -1,10 +1,13 @@
 package com.xxx.webboot.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.xxx.webboot.entity.Student;
 
 @Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+//public interface StudentRepository extends CrudRepository<Student, Long> {
 }
