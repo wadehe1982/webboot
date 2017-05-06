7a312f2095300a0bfe2896fedee3898723914ce1
wei <ir_01@163.com>
2017-05-06 21:31:46 +0800

    remove unused import.

--- src/main/java/com/xxx/webboot/repository/StudentRepository.java
+++ src/main/java/com/xxx/webboot/repository/StudentRepository.java
@@ -1,7 +1,6 @@
 package com.xxx.webboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.xxx.webboot.entity.Student;
