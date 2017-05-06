6ac58c5df034afddb11f88c02788a12be084d098
wei <ir_01@163.com>
2017-05-06 17:51:43 +0800

    Add spring cloud config. consider this as a config client.

---
+++ src/main/java/com/xxx/webboot/controller/CloudConfigController.java
@@ -0,0 +1,39 @@
+package com.xxx.webboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("config")
+//make sure the config items in git can be refreshed when invoke /refresh
+//Note: To refresh it, need send a POST request.
+//Example: curl -d "" "http://localhost:8080/myboot/refresh"
+@RefreshScope 
+public class CloudConfigController {
+	
+	/*
+	 * Two ways to get properties configuration items
+	 * first is this one: auto wired a Environment object
+	 * and then getProperty.
+	 * The second way is use @Value annotation.
+	 * 
+	 * Note: If this failed to load properties, consider start up
+	 * Spring cloud config side configserver or just disable this.
+	 */
+	@Autowired
+	private Environment environment;
+	
+	@Value("${mysqldb.datasource.username}")
+	private String name;
+	
+	@RequestMapping(name = "test")
+	public String test(){
+		System.out.println(environment.getProperty("name"));
+		return name;
+	}
+	
+}
